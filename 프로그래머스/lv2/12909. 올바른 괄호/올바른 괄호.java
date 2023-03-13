f1d86eb17895cb06b64751740bc2dc72718e4787
hobambi <122279703+hobambi@users.noreply.github.com>
2023-03-13 09:31:19 +0900

    [level 2] Title: 올바른 괄호, Time: 16.84 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='('){
+                stack.add("(");
+            }else {
+                if(stack.isEmpty())
+                    return false;
+                stack.pop();
+            }
+        }
+        
+        if(!stack.isEmpty())
+            return false;
+        return answer;
+    }
+}
